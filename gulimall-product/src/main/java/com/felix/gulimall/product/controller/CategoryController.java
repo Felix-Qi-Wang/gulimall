852a57286a9d0c5719357ee1f285f6795b3b82d5
felix-qi-wang <qwan2995@uni.sydney.edu.au>
2022-12-13 23:51:33 +0800

    add aliyun OSS service, and third-party module, use OOS service to store images now

--- gulimall-product/src/main/java/com/felix/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/felix/gulimall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.felix.gulimall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -31,15 +32,17 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
-    /**
-     * 列表
-     */
-    @RequestMapping("/list")
-    //@RequiresPermissions("product:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
 
-        return R.ok().put("page", page);
+
+    /**
+     * 查出所有分类以及子分类，以树形结构组装起来
+     */
+    @RequestMapping("/list/tree")
+    // @RequiresPermissions("product:category:list")
+    public R list(@RequestParam Map<String, Object> params){
+        List<CategoryEntity> entities = categoryService.listWithTree();
+
+        return R.ok().put("data", entities);
     }
 
 
@@ -47,12 +50,14 @@ public class CategoryController {
      * 信息
      */
     @RequestMapping("/info/{catId}")
-    //@RequiresPermissions("product:category:info")
+    // @RequiresPermissions("product:category:info")
     public R info(@PathVariable("catId") Long catId){
-		CategoryEntity category = categoryService.getById(catId);
+        CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
+
+
 
     /**
      * 保存
@@ -62,6 +67,17 @@ public class CategoryController {
     public R save(@RequestBody CategoryEntity category){
 		categoryService.save(category);
 
+        return R.ok();
+    }
+
+    /**
+     * 修改分类
+     */
+    @RequestMapping("/update/sort")
+    // @RequiresPermissions("product:category:update")
+    public R update(@RequestBody CategoryEntity[] category){
+
+        categoryService.updateBatchById(Arrays.asList(category));
         return R.ok();
     }
 
@@ -76,15 +92,20 @@ public class CategoryController {
         return R.ok();
     }
 
+
     /**
      * 删除
+     * @RequestBody获取请求体，必须发送POST请求
+     * SpringMVC自动将请求体的数据(json),转为对应的对象
      */
     @RequestMapping("/delete")
-    //@RequiresPermissions("product:category:delete")
+    // @RequiresPermissions("product:category:delete")
     public R delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
 
+        categoryService.removeMenuByIds(Arrays.asList(catIds));
         return R.ok();
     }
+
+
 
 }
---
+++ gulimall-third-party/src/test/java/com/felix/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -0,0 +1,40 @@
+package com.felix.gulimall.thirdparty;
+
+import com.aliyun.oss.OSSClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+@SpringBootTest
+class GulimallThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+    @Autowired
+    OSSClient ossClient;
+    @Test
+    public void testUpload() throws FileNotFoundException {
+        // Endpoint以杭州为例，其它Region请按实际情况填写。
+//        String endpoint = "oss-cn-beijing.aliyuncs.com";
+//        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+//        String accessKeyId = "LTAI5tQYzjQ1krUE2vJ6XQCA"; //自己知道就好，我的暴露安全性被攻击了
+//        String accessKeySecret = "UEd0i4Zcd3Pa3pQcPRriwIhDgESrEz";
+//
+//        // 创建OSSClient实例。
+//        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        // 上传文件流。
+        InputStream inputStream = new FileInputStream("C:\\Users\\79142\\Pictures\\63804992915079.png");
+        ossClient.putObject("gulimall-imagez", "63804992915079again.png", inputStream);
+
+        // 关闭OSSClient。
+        ossClient.shutdown();
+        System.out.println("上传成功.");
+    }
+
+}
