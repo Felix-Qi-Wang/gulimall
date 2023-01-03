1ee7f1486e2656c45843b8ca14ab88d1d7435f5b
felix-qi-wang <qwan2995@uni.sydney.edu.au>
2023-01-03 22:18:36 +0800

    更新了search的一些配置，fixed some bugs

--- gulimall-search/src/main/java/com/felix/gulimall/search/controller/SearchController.java
+++ gulimall-search/src/main/java/com/felix/gulimall/search/controller/SearchController.java
@@ -13,8 +13,8 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class SearchController {
 
-    @Autowired
-    private MallSearchService mallSearchService;
+   // @Autowired
+    //private MallSearchService mallSearchService;
 
     /**
      * 自动将页面提交过来的所有请求参数封装成我们指定的对象
@@ -27,9 +27,9 @@ public class SearchController {
         param.set_queryString(request.getQueryString());
 
         //1、根据传递来的页面的查询参数，去es中检索商品
-        SearchResult result = mallSearchService.search(param);
+       // SearchResult result = mallSearchService.search(param);
 
-        model.addAttribute("result",result);
+       // model.addAttribute("result",result);
 
         return "list";
     }
